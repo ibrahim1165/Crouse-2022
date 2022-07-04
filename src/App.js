4d60ab894b01255babaa7138f89c7726b9e4477b
Md Ibrahim <mdrafi15429@gmail.com>
2022-07-04 13:02:58 +0600

    testimonal section added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Pages/Navbar/Navbar';
 import Header from './Pages/Header/Header';
 import Services from './Pages/Crouse/Services';
 import HearoSection from './Pages/HeroSection/HearoSection';
+import Testimonial from './Pages/Tastemonial/Testimonial';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
     <Header />
     <Services />
     <HearoSection />
+    <Testimonial />
 
 
 
